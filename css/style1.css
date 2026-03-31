97f1b0662d4b1b6902cfb069d611281f07a40684
nhat <nhatmanh002@gmail.com>
2026-04-01 00:37:56 +0700

    Create a project structure

---
+++ css/style1.css
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="vi">
+	<head>
+		<title>làm web </title>
+		<meta charset = "utf-8">
+	</head>
+	<body>
+		<ul>
+			<li>	
+				<a href = "html/page1.html">
+                                thông tin cá nhân
+				</a>
+			</li>
+		</ul>
+	</body>
+</html>
